99886d33385d3c44a65018c62a100bdde83e06ee
FCF5646448 <2998000457@qq.com>
2020-07-28 07:47:20 +0800

    添加ffmpeg demo

---
+++ FFmpeg/音视频入门/MyApp/MyApp/file/test.h
@@ -0,0 +1,19 @@
+//
+//  test.h
+//  MyApp
+//
+//  Created by 冯才凡 on 2020/7/27.
+//  Copyright © 2020 冯才凡. All rights reserved.
+//
+
+#ifndef test_h
+#define test_h
+
+#include <stdio.h>
+
+
+void test(void);
+
+void capAudio(void);
+
+#endif /* test_h */
