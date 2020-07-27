8b7d40a74682b508122a23812591c457bd2f596a
FCF5646448 <2998000457@qq.com>
2020-07-28 00:21:56 +0800

    ffmpeg 入门

---
+++ FFmpeg/音视频入门/MyApp/MyApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MyApp
+//
+//  Created by 冯才凡 on 2020/7/27.
+//  Copyright © 2020 冯才凡. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ FFmpeg/音视频入门/MyApp/MyApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by 冯才凡 on 2020/7/27.
+//  Copyright © 2020 冯才凡. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        capAudio()
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
