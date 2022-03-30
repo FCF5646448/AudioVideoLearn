882f8acd679cd6d2696a1fa041131d3640aee6e7
FCF5646448 <2998000457@qq.com>
2022-03-31 07:43:28 +0800

    绘制Window

--- 1、OpenGL/Demos/OpenGL001/OpenGL001/main.cpp
+++ 1、OpenGL/Demos/OpenGL001/OpenGL001/main.cpp
@@ -25,26 +25,31 @@ int main(int argc, char * argv[]) {
     GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
     if (window == NULL) {
         printf("Failed to create GLFW window");
-        glfwTerminate();
+        glfwTerminate(); // 关闭所有资源
         return -1;
     }
     // 将窗口上下文设置成当前线程上下文
     glfwMakeContextCurrent(window);
     
-    // 设置视口调整大小时的回调
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    
-    // 初始化GLAD
+    // 初始化GLAD，调用OpenGL函数之前需先进行GLAD的初始化
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         printf("failed to initialize GLAD");
         return -1;
     }
     
-    // 循环不退出
+    // 注册窗口变化监听函数。
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    
+    
+    // 渲染循环
+    // glfwWindowShouldClose 判断glfw是否将要关闭。如果将要关闭，就交换缓存区。
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
         
         // 渲染指令
+        // glClearColor 是一个状态设置函数，用来设置清空屏幕所用的颜色。
+        // glClear 是一个状态使用函数，它所接受的参数是一个buffer。这里表示用glClearColor设置的颜色来清空当前window。
+        // 这里需要额外理解清空的意思：因为clear并不是移除掉了window，而只是将window里的内容清除掉。假设不调用clearColor函数，直接调用clear，最终window是被设置成了默认的黑色。
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         
@@ -67,7 +72,9 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
 
 void processInput(GLFWwindow *window) {
     // 检查是否按下了esc键
+    // glfwGetKey 会判断在当前窗口状态下，第二个参数（一个按键）是否被按下
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        // 将窗口 shouldClose设置为true
         glfwSetWindowShouldClose(window, true);
     }
 }
