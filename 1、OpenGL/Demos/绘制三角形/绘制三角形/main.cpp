24dd543828bec44220ee26bfcd7ac7f95e1fd415
FCF5646448 <2998000457@qq.com>
2022-05-10 08:14:26 +0800

    完成三角形绘制

--- 1、OpenGL/Demos/绘制三角形/绘制三角形/main.cpp
+++ 1、OpenGL/Demos/绘制三角形/绘制三角形/main.cpp
@@ -9,11 +9,29 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#include "Triangle.hpp"
-
 void observerWindowFrameChanged(GLFWwindow* window, int width, int height);
 void observerInput(GLFWwindow* window);
-void drawTriangle();
+
+// settings
+// 窗口初始化宽高
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+// glsl 着色器源码: 顶点着色器
+const char * vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+// GLSL 片段着色器
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n";
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
@@ -23,31 +41,103 @@ int main(int argc, const char * argv[]) {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     
-    GLFWwindow* window = glfwCreateWindow(800, 600, "绘制三角形", NULL, NULL);
+#ifdef  __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+    
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "绘制三角形", NULL, NULL);
     if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return  -1;
     }
     
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, observerWindowFrameChanged);
     
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cout << "初始化glad失败";
         return -1;
     }
     
-    glfwSetFramebufferSizeCallback(window, observerWindowFrameChanged);
     
-    float test[] = {
-       -0.5f, -0.5f, 0.0f,
-       0.5f, -0.5f, 0.0f,
-       0.0f, 0.5f, 0.0f
+    // -------------------------------------------------------
+    // 顶点着色器
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER); // 创建着色器程序
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL); // 将着色器源码附着在着色器对象上
+    glCompileShader(vertexShader); // 编译
+    
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success); // 获取编译结果
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "error::SHADER::VERTEXT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    
+    // 片段着色器
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    
+    // 查看编译结果
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "error::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    
+    
+    // 连接着色器。
+    // 着色器程序: 将着色器程序连接成着色器程序对象
+    unsigned int shaderProgram = glCreateProgram();
+    // 将着色器附加到程序对象上
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    // 连接
+    glLinkProgram(shaderProgram);
+    
+    // 查看连接结果
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "error::SHADER::PROGRAM::LINK_FAILED\n" << infoLog << std::endl;
+    }
+    
+    // 连接完成后就可以删除各个着色器了。
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    
+    // 设置顶点数据（和缓冲区）并配置顶点属性
+    // --------------------------------------------------------
+    float vertices[] = {
+       -0.5f, -0.5f, 0.0f, // left
+       0.5f, -0.5f, 0.0f, // right
+       0.0f, 0.5f, 0.0f // top
     };
-    Triangle triangle;
-    triangle.drawTriangle(test);
     
+    // 连接顶点属性
+    // 0 生成顶点缓冲对象VBO，第二个参数就是缓存区独一无二的ID
+    unsigned int VBO, VAO;
+    glGenBuffers(1, &VBO);
+    glGenVertexArrays(1, &VAO);
+    // 绑定VAO对象
+    glBindVertexArray(VAO);
+    
+    // 绑定到缓冲
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // 将顶点数据复制到缓存内存中：第4个参数是表示如果管理给定的顶点数据。一共有3种类型：GL_STATIC_DRAW：不变，GL_DYNAMIC_DRAW:动态的；GL_STREAM_DRAW：每次绘制都会改变
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0); // 设置顶点属性指针
+    glEnableVertexAttribArray(0); // 启用顶点属性
+    
+    // 解绑 VBO 和 VAO
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+    
+
     while (!glfwWindowShouldClose(window)) {
         
         observerInput(window);
@@ -55,13 +145,19 @@ int main(int argc, const char * argv[]) {
         glClearColor(0.2f, 0.3f, 0.3f, 0.3f);
         glClear(GL_COLOR_BUFFER_BIT);
         
-        triangle.draw();
+        // 激活 相当于将顶点数据发送给GPU了
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
     
         glfwSwapBuffers(window); // 交换缓存
         glfwPollEvents(); // 事件检测
     }
     
-    
+    // 取消资源分配
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
     
     glfwTerminate();
     return 0;
@@ -73,10 +169,6 @@ void observerWindowFrameChanged(GLFWwindow* window, int width, int height) {
 
 void observerInput(GLFWwindow* window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE)) {
-//        glfwSetWindowShouldClose(window, true);
+        glfwSetWindowShouldClose(window, true);
     }
-}
-
-void drawTriangle() {
-    
 }
